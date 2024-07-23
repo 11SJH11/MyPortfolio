cd6456e88cc0c96aacfd2ffb002507b98a855864
11SJH11 <jamieholtm1@gmail.com>
2024-07-23 12:02:22 +0100

    updated page (index+css+js)

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+const toggleThemeButton = document.querySelector(".dark-mode-button")
+
+function handleclick(){
+    const isDark = document.body.classList.toggle("dark-mode");
+    const isSrc = isDarkMode;
+}
+
+
+toggleThemeButton.addEventListener("click", handleclick)
